a3962bfc36fedd5d7e18e726493ed4d11cfa4458
EC2 Default User <ec2-user@ip-10-131-1-122.ec2.internal>
2016-01-26 02:15:31 +0000

    [script] Updated qc snakefile scripts

--- wgs/illumina/qc/pull_files.py
+++ wgs/illumina/qc/pull_files.py
@@ -1,21 +1,27 @@
 #!/bin/python
 
-import boto3
+#import boto3
+import os
+import argparse
+
 client=boto3.client('s3','us-east-1')
 from boto3.s3.transfer import S3Transfer
 from boto3.s3.transfer import TransferConfig
 from boto3 import utils
 
-
-config = TransferConfig(
-    multipart_threshold=50 * 1024 * 1024,
-    multipart_chunksize=50 * 1024 * 1024,
-    max_concurrency=10,
-    num_download_attempts=10,
-)
-
-
-transfer=S3Transfer(client, config)
-transfer.download_file('sscwgs','11194/Sample_SSC00668/VCF/mtDNA/SSC00668.mtDNAcn.txt','SSC00668.mtDNAcn.txt',extra_args={'RequestPayer':'requester'})
+def copy_from_requester_pay_bucket(sample_file=None):
+	config = TransferConfig(
+	multipart_threshold=25 * 1024 * 1024,\
+        multipart_chunksize=25 * 1024 * 1024,\
+        max_concurrency=10,num_download_attempts=10,)
+	transfer=S3Transfer(client, config)
+	sample_file=sample_file
+	os.makedirs((os.path.dirname(sample_file)))
+	transfer.download_file('sscwgs',sample_file,sample_file,extra_args={'RequestPayer':'requester'})
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sample_file", action="store", required=True)
+    args = parser.parse_args()
+    copy_from_requester_pay_bucket(args.sample_file)
